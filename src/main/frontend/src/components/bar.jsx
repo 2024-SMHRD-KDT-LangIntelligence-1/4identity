8f5e6fe21d29cf824b941f7f195c3a4de6390248
가을 <jge@ga-eul-ui-MacBookPro.local>
2025-03-26 11:19:11 +0900

    윤진  수정11:19

--- src/main/frontend/src/components/bar.jsx
+++ src/main/frontend/src/components/bar.jsx
@@ -14,7 +14,7 @@ function Header() {
         <div className="headerBox">
           <div className="logo">4identity</div>
 
-          <div className="right-side">
+          <div className="rightSide">
             <div className="headerBtn">
               <button onClick={() => navigate("/login")}>로그인</button>
               <button onClick={() => navigate("/signup")}>회원가입</button>
